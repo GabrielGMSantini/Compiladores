ee6ebb47d26b25277bc16adbd4f696e4aac81aa2
GabrielGMSantini <53902751+GabrielGMSantini@users.noreply.github.com>
2022-11-22 14:06:48 -0300

    Começo da execução
    Comandos de Start, Alloc, Dalloc, Call, Halt, Read e Print feitos

--- VM.c
+++ VM.c
@@ -3,14 +3,25 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define TAMANHO_DE_MEMORIA 200
 
 FILE* fptr;
 
 char P[100][26];
 
-int M[200];
+int M[TAMANHO_DE_MEMORIA];
 int s;
 int i;
+
+//Printa a Stack inteira
+int PrintStack(){
+	int j = TAMANHO_DE_MEMORIA;
+	for(;j>=0;j--){
+		printf("%d\n",M[j]);
+	}
+	return 0;
+}
+
 
 int LoadFile(char* filename){
 	if ((fptr = fopen(filename, "r")) == NULL) {
@@ -100,10 +111,74 @@ int Exec(int l){
 		comando[j] = P[l][j+4];	
 	}
 	comando[8] = '\0';
+	//Se o comando for START
+	if(!strcmp(comando,"START   ")){
+		printf("start");
+		s=-1;
+	}
+	else{
+		//Se for alocacao
+		if(!strcmp(comando,"ALLOC   ")){
+			int m,n,k;
+			m=atoi(param1);
+			n=atoi(param2);
+			printf("alloc %d %d",m,n);
+			for(k=0;k<n;k++){
+				s++;
+				M[s]=M[m+k];
+			}
+		}
+		else{
+			//Se for dealocacao
+			if(!strcmp(comando,"DALLOC  ")){
+				int m,n,k;
+				m=atoi(param1);
+				n=atoi(param2);
+				for(k=n-1;k>=0;k--){
+					M[m+k]= M[s];
+					s--;
+				}
+			}
+			else{
+				//Se for chamada
+				if(!strcmp(comando,"CALL    ")){
+					s++;
+					M[s]=i+1;
+					i=atoi(param1);
+				}
+				else{
+					//Se for retorno
+					if(!strcmp(comando,"RETURN  ")){
+						i=M[s];
+						s--;
+					}
+					else{
+						//Se for HALT
+						if(!strcmp(comando,"HLT     ")){
+							return -1;
+						}
+						else{
+							//Se for leitura
+							if(!strcmp(comando,"RD      ")){
+								s++;
+								scanf("%d",M[s]);
+							}
+							else{
+								if(!strcmp(comando,"PRN     ")){
+									printf("%d",M[s]);
+									s--;
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	
 	
 	return 0;	
 }
-
 
 int main(int argc, char** argv){
 	int r = LoadFile("./gera1.obj");
@@ -111,5 +186,9 @@ int main(int argc, char** argv){
 	for(;j<r;j++){
 		puts(P[j]);
 	}
-	printf("%d",FetchLabel(2));
+	for(j=0;j<TAMANHO_DE_MEMORIA;j++){
+		M[j]=0;
+	}
+	Exec(0);
+	Exec(1);
 }
