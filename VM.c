cd5ac781a4fa13d0ade0783177bd3a9d6a0516b3
GabrielGMSantini <53902751+GabrielGMSantini@users.noreply.github.com>
2022-11-23 09:43:51 -0300

    Update VM.c

--- VM.c
+++ VM.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define TAMANHO_DE_MEMORIA 200
+#define TAMANHO_DE_MEMORIA 50
 
 FILE* fptr;
 
@@ -113,8 +113,9 @@ int Exec(int l){
 	comando[8] = '\0';
 	//Se o comando for START
 	if(!strcmp(comando,"START   ")){
-		printf("start");
+		printf("start\n");
 		s=-1;
+		i++;
 	}
 	else{
 		//Se for alocacao
@@ -122,11 +123,12 @@ int Exec(int l){
 			int m,n,k;
 			m=atoi(param1);
 			n=atoi(param2);
-			printf("alloc %d %d",m,n);
+			printf("alloc %d %d\n",m,n);
 			for(k=0;k<n;k++){
 				s++;
 				M[s]=M[m+k];
 			}
+			i++;
 		}
 		else{
 			//Se for dealocacao
@@ -134,39 +136,259 @@ int Exec(int l){
 				int m,n,k;
 				m=atoi(param1);
 				n=atoi(param2);
+				printf("dalloc %d %d\n",m,n);
 				for(k=n-1;k>=0;k--){
 					M[m+k]= M[s];
 					s--;
 				}
+				i++;
 			}
 			else{
 				//Se for chamada
 				if(!strcmp(comando,"CALL    ")){
+					int p;
 					s++;
 					M[s]=i+1;
-					i=atoi(param1);
+					p=atoi(param1);
+					i = FetchLabel(p);
+					printf("call %d\n",p);
 				}
 				else{
 					//Se for retorno
 					if(!strcmp(comando,"RETURN  ")){
+						printf("return %d\n",s);
 						i=M[s];
 						s--;
 					}
 					else{
 						//Se for HALT
 						if(!strcmp(comando,"HLT     ")){
+							printf("Halt\n");
 							return -1;
 						}
 						else{
 							//Se for leitura
 							if(!strcmp(comando,"RD      ")){
+								printf("read\n");
 								s++;
-								scanf("%d",M[s]);
+								scanf("%d",&M[s]);
+								i++;
 							}
 							else{
+								//Se for escrita
 								if(!strcmp(comando,"PRN     ")){
-									printf("%d",M[s]);
+									printf("print %d\n",M[s]);
 									s--;
+									i++;
+								}
+								else{
+									//Se for carregar constante
+									if(!strcmp(comando,"LDC     ")){
+										int k = atoi(param1);
+										printf("%d",k);
+										s++;
+										M[s] = k;
+										i++;
+									}
+									else{
+										//Se for carregar variavel
+										if(!strcmp(comando,"LDV     ")){
+											int n = atoi(param1);
+											s++;
+											M[s] = M[n];
+											i++;
+										}
+										else{
+											//Se for adicao
+											if(!strcmp(comando,"ADD     ")){
+												M[s-1] = M[s-1] + M[s];
+												s--;
+												i++;
+											}
+											else{
+												//Se for subtracao
+												if(!strcmp(comando,"SUB     ")){
+													M[s-1] = M[s-1] - M[s];
+													s--;
+													i++;
+												}
+												else{
+													//Se for multiplicacao
+													if(!strcmp(comando,"MULT    ")){
+														M[s-1] = M[s-1] * M[s];
+														s--;
+														i++;
+													}
+													else{
+														//Se for divisao
+														if(!strcmp(comando,"DIVI    ")){
+															M[s-1] = M[s-1] / M[s];
+															s--;
+															i++;
+														}
+														else{
+															//Se for inverter sinal
+															if(!strcmp(comando,"INV     ")){
+																M[s] = -(M[s]);
+																i++;
+															}
+															else{
+																//Se for "e"
+																if(!strcmp(comando,"AND     ")){
+																	if(M[s-1] == 1 && M[s] == 1){
+																		M[s-1] = 1;
+																	}
+																	else{
+																		M[s-1] = 0;
+																	}
+																	s--;
+																	i++;
+																}
+																else{
+																	//Se for "ou"
+																	if(!strcmp(comando,"OR      ")){
+																		if(M[s-1] == 1 || M[s] == 1){
+																			M[s-1] = 1;
+																		}
+																		else{
+																			M[s-1] = 0;
+																		}
+																		s--;
+																		i++;
+																	}
+																	else{
+																		//Se for negacao
+																		if(!strcmp(comando,"NEG     ")){
+																			M[s] = 1-M[s];
+																			i++;
+																		}
+																		else{
+																			//Se for menor
+																			if(!strcmp(comando,"CME     ")){
+																				if(M[s-1] < M[s]){
+																					M[s-1] = 1;
+																				}
+																				else{
+																					M[s-1] = 0;
+																				}
+																				s--;
+																				i++;
+																			}
+																			else{
+																				//Se for maior
+																				if(!strcmp(comando,"CMA     ")){
+																					if(M[s-1] > M[s]){
+																						M[s-1] = 1;
+																					}
+																					else{
+																						M[s-1] = 0;
+																					}
+																					s--;
+																					i++;
+																				}
+																				else{
+																					//Se for igual
+																					if(!strcmp(comando,"CEQ     ")){
+																						if(M[s-1] == M[s]){
+																							M[s-1] = 1;
+																						}
+																						else{
+																							M[s-1] = 0;
+																						}
+																						s--;
+																						i++;
+																					}
+																					else{
+																						//Se for nao igual
+																						if(!strcmp(comando,"CDIF    ")){
+																							if(M[s-1] != M[s]){
+																								M[s-1] = 1;
+																							}
+																							else{
+																								M[s-1] = 0;
+																							}
+																							s--;
+																							i++;
+																						}
+																						else{
+																							//Se for menor ou igual
+																							if(!strcmp(comando,"CMEQ    ")){
+																								if(M[s-1] <= M[s]){
+																									M[s-1] = 1;
+																								}
+																								else{
+																									M[s-1] = 0;
+																								}
+																								s--;
+																								i++;
+																							}
+																							else{
+																								//Se for maior ou igual
+																								if(!strcmp(comando,"CMAQ    ")){
+																									if(M[s-1] >= M[s]){
+																										M[s-1] = 1;
+																									}
+																									else{
+																										M[s-1] = 0;
+																									}
+																									s--;
+																									i++;
+																								}
+																								else{
+																									//Se for armazenamento
+																									if(!strcmp(comando,"STR     ")){
+																										int n;
+																										n = atoi(param1);
+																										M[n] = M[s];
+																										s--;
+																										i++;
+																									}
+																									else{
+																										//Se for pulo
+																										if(!strcmp(comando,"JMP     ")){
+																											int p;
+																											p = atoi(param1);
+																											i = FetchLabel(p);
+																										}
+																										else{
+																											//Se for pulo quando false
+																											if(!strcmp(comando,"JMPF    ")){
+																												if(M[s] == 0){
+																													int p = atoi(param1);
+																													i = FetchLabel(p);
+																												}
+																												else{
+																													i++;
+																												}
+																												s--;
+																											}
+																											else{
+																												//Se nao for null
+																												if(strcmp(comando,"NULL    ")){
+																													return 1;
+																												}
+																												else{
+																													i++;
+																												}
+																											}
+																										}
+																									}
+																								}	
+																							}
+																						}
+																					}
+																				}
+																			}
+																		}
+																	}
+																}
+															}
+														}
+													}
+												}
+											}
+										}
+									}
 								}
 							}
 						}
@@ -189,4 +411,10 @@ int main(int argc, char** argv){
 	for(j=0;j<TAMANHO_DE_MEMORIA;j++){
 		M[j]=0;
 	}
+	int t;
+	do{
+		printf("\n\ni=%d\ns=%d\n\n",i,s);
+		t=Exec(i);
+		PrintStack();
+	}while(t == 0);
 }
